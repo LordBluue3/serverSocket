7393a8affe7021fdda6dab8fa827820735fca770
LordBluue3 <mikaelcandido71@gmail.com>
2022-03-20 14:17:06 -0300

    Criado um server socket para a inração com a aplicação

---
+++ src/main/java/lord/serverSocket/ServerSockett.java
@@ -0,0 +1,26 @@
+package lord.serverSocket;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.Date;
+
+public class ServerSockett  {
+
+    public static void main(String[] args) throws IOException {
+
+           ServerSocket server = new ServerSocket(12345);
+
+           while(true){
+
+               Socket cliente = server.accept();
+               System.out.println("Cliente conectado: " + cliente.getInetAddress().getHostAddress());
+               ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
+               saida.flush();
+               saida.close();
+               cliente.close();
+
+           }
+    }
+}
