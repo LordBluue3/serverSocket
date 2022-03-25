60f4222f94eb7a05550382fdd6232b8ade9bc36b
LordBluue3 <mikaelcandido71@gmail.com>
2022-03-25 13:10:59 -0300

    Criando class Usuario e seus getters e construtor

---
+++ src/main/java/lord/serverSocket/database/Usuario.java
@@ -0,0 +1,19 @@
+package lord.serverSocket.database;
+
+public class Usuario {
+    private String email, senha;
+
+    public Usuario(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+}
