6e67e4e1604c4af55e1acf72c9caf950674d1062
LordBluue3 <mikaelcandido71@gmail.com>
2022-03-27 22:58:34 -0300

    Refatorando o código e criando novas funcionalidades

--- src/main/java/lord/serverSocket/ServerSockett.java
+++ src/main/java/lord/serverSocket/ServerSockett.java
@@ -3,7 +3,9 @@ package lord.serverSocket;
 import com.henryfabio.sqlprovider.executor.SQLExecutor;
 import lord.serverSocket.database.SqlProvedor;
 import lord.serverSocket.database.Usuario;
-import lord.serverSocket.database.UsuarioAdapter;
+import lord.serverSocket.interacao.Mensagens;
+import lord.serverSocket.seguranca.Consulta;
+import lord.serverSocket.seguranca.Validar;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -11,6 +13,7 @@ import org.json.simple.parser.ParseException;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -20,52 +23,26 @@ public class ServerSockett  {
 
            ServerSocket ss = new ServerSocket(12345);
            SQLExecutor executor = new SqlProvedor().criandoBd();
-
+           List<String> listOfMessages = null;
+           Socket socket;
 
            while(true) {
+               System.out.println("ServerSocket está conectando...");
+               System.out.println("Server conectado, aguardando requisições!!!");
 
-               // don't need to specify a hostname, it will be the current machine
-               System.out.println("ServerSocket awaiting connections...");
-               Socket socket = ss.accept(); // blocking call, this will wait until a connection is attempted on this port.
-               System.out.println("Connection from " + socket + "!");
+               socket = ss.accept(); //faz com que o while não continue a menos que uma mensagem seja requisitada
+               System.out.println("Conectado ao " + socket + "!");
 
-               // get the input stream from the connected socket
-               InputStream inputStream = socket.getInputStream();
-               // create a DataInputStream so we can read data from it.
-               ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+               //ler mensagem do cliente
+               List listOfmessagen = new ArrayList<String>();
+               Mensagens mensagem = new Mensagens(socket, listOfMessages);
+               mensagem.lerMensagens();
 
-               // read the list of messages from the socket
-               List<String> listOfMessages = null;
-               try {
-                   listOfMessages = (List<String>) objectInputStream.readObject();
-               } catch (ClassNotFoundException e) {
-                   e.printStackTrace();
-               }
-               System.out.println("Received [" + listOfMessages.size() + "] messages from: " + socket);
-               // print out the text of every message
-               System.out.println("All messages:");
-               for (String msg : listOfMessages) {
-                   try {
-                       JSONObject parsed = (JSONObject) new JSONParser().parse(msg);
-                       System.out.println("Mensagem: ");
-                       String login = (String) parsed.get("login");
-                       String senha = (String) parsed.get("senha");
+               // imprimir o texto de cada mensagem
+               System.out.println("Todas as mensagens:");
+               mensagem.imprimirMensagem(executor,ss);
 
-                       Usuario usuario = executor.resultOneQuery("SELECT * FROM tbUsuarios where email =? and senha =?", s ->{
-                           s.set(1,login);
-                           s.set(2,senha);
-                       }, UsuarioAdapter.class);
-                       if(usuario == null){
-                           System.out.println("Valor null");
-                       }else {
-                           System.out.println("Acesso permitido");
-                       }
-                       System.out.println("Closing sockets.");
-                   } catch (ParseException e) {
-                       e.printStackTrace();
-                   }
-                   socket.close();
+               socket.close();
                }
            }
     }
-}
