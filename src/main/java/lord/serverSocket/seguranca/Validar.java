b809dff5af1297c7c5fff60aca49a2642790d472
LordBluue3 <mikaelcandido71@gmail.com>
2022-03-27 23:03:06 -0300

    Envia o resultado da pesquisa no banco de dados para o cliente

---
+++ src/main/java/lord/serverSocket/seguranca/Validar.java
@@ -0,0 +1,38 @@
+package lord.serverSocket.seguranca;
+
+import lord.serverSocket.database.Usuario;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Validar {
+    private final Socket socket;
+    private final Usuario usuario;
+
+    public Validar(Socket socket, Usuario usuario) {
+        this.socket = socket;
+        this.usuario = usuario;
+    }
+    public void validar(){
+
+        //Enviando mensagem para o cliente
+        DataOutputStream dos = null;
+        try {
+            dos = new DataOutputStream(socket.getOutputStream());
+            if(usuario == null){
+                System.out.println("Login inválido");
+                dos.writeUTF("Login inválido");
+            }else {
+                System.out.println("Logado com sucesso!!");
+                dos.writeUTF("Logado com sucesso");
+            }
+            System.out.println("Fechando sockets.");
+            System.out.println("=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
