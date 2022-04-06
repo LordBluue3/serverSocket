d3ce08658d10f9abf46b6906a6c66c1b56236394
LordBluue3 <mikaelcandido71@gmail.com>
2022-04-05 22:12:39 -0300

    Adicionado sistema de contador no ServerSocket

--- src/main/java/lord/serverSocket/seguranca/Validar.java
+++ src/main/java/lord/serverSocket/seguranca/Validar.java
@@ -1,28 +1,47 @@
 package lord.serverSocket.seguranca;
 
+import lord.serverSocket.ServerSockett;
 import lord.serverSocket.database.Usuario;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-
 public class Validar {
     private final Socket socket;
     private final Usuario usuario;
 
+
+
+
+
     public Validar(Socket socket, Usuario usuario) {
         this.socket = socket;
         this.usuario = usuario;
+
     }
+
     public void validar(){
+
 
         //Enviando mensagem para o cliente
         DataOutputStream dos = null;
+        System.out.print("Enviando a resposta: ");
+
         try {
             dos = new DataOutputStream(socket.getOutputStream());
+
             if(usuario == null){
                 System.out.println("Login inválido");
                 dos.writeUTF("Login inválido");
+                    dos.writeUTF(String.valueOf(ServerSockett.contador));
+                    System.out.println("numero da tentativa: "+ServerSockett.contador);
+
+                    if(ServerSockett.contador == 3){
+                        ServerSockett.contador = 1;
+                    }else {
+                         ServerSockett.contador =  ServerSockett.contador+1;
+                    }
+
             }else {
                 System.out.println("Logado com sucesso!!");
                 dos.writeUTF("Logado com sucesso");
@@ -32,7 +51,7 @@ public class Validar {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
     }
 
-}
